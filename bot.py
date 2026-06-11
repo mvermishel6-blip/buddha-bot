35308c5aabeeb0dfcd35a3629286d3a958712ed8
mvermishel6-blip <mvermishel6@gmail.com>
2026-06-11 09:38:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -169,11 +169,13 @@ async def handle(callback: CallbackQuery):
         save_data(data)
 
         await callback.message.answer("💌 Лайк отправлен!")
-                  if target_id in users:
+
+        if target_id in users:
             await bot.send_message(
                 target_id,
                 f"❤️ Кому-то понравилась твоя анкета!\n\n{users[uid]}"
             )
+                    return
         # взаимный лайк
         
         if target_id in likes and uid in likes[target_id]:
